568c5a7e8d8edf82eb2ecd8a202fb62dce18076d
Gustavseriksen <guer0001@stud.kea.dk>
2026-04-13 23:21:15 +0200

    Add User entity and UserRepository

---
+++ spark-api/src/main/java/com/gustaveriksen/spark/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.gustaveriksen.spark.repository;
+
+import com.gustaveriksen.spark.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    
+    // Spring Data JPA magically writes the SQL query for this method behind the scenes!
+    Optional<User> findByEmail(String email);
+    
+    boolean existsByEmail(String email);
+}
