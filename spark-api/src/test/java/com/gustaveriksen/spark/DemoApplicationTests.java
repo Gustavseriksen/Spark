6c3f36ab15a7cc5bc0329c91b4aa6a1569c970a3
Gustavseriksen <guer0001@stud.kea.dk>
2026-04-13 23:45:10 +0200

    Fix CI test by injecting H2 properties directly into SpringBootTest

--- spark-api/src/test/java/com/gustaveriksen/spark/DemoApplicationTests.java
+++ spark-api/src/test/java/com/gustaveriksen/spark/DemoApplicationTests.java
@@ -3,11 +3,19 @@ package com.gustaveriksen.spark;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// We feed the H2 fake database settings directly into the test itself!
+@SpringBootTest(properties = {
+    "spring.datasource.url=jdbc:h2:mem:testdb",
+    "spring.datasource.driverClassName=org.h2.Driver",
+    "spring.datasource.username=sa",
+    "spring.datasource.password=",
+    "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect"
+})
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        // If this test passes, it means the application successfully booted up!
+    }
 
 }
